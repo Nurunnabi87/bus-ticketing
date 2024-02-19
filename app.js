18d39d3ead3556c5f84fd8879a133e046fd043e6
Nurunnabi87 <nurunnabi.jewel87@gmail.com>
2024-02-19 18:56:56 +0600

    functionalaties added

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
-let selectedSeats = [];
-function handleSeatClickedEvent(event) {
-  const passengerPressed = event.target.innerText;
-  console.log(passengerPressed);
+// let selectedSeats = [];
+// function handleSeatClickedEvent(event) {
+//   const passengerPressed = event.target.innerText;
+//   console.log(passengerPressed);
 
-  selectedSeats.push(passengerPressed);
+//   selectedSeats.push(passengerPressed);
 
-  console.log(selectedSeats);
-  // const currentSeatElement = document.getElementsByClassName("seat-number");
+//   console.log(selectedSeats);
+// const currentSeatElement = document.getElementsByClassName("seat-number");
 
-  // const currentSeat = currentSeatElement.innerText;
-  // console.log(passengerPressed, currentSeat);
+// const currentSeat = currentSeatElement.innerText;
+// console.log(passengerPressed, currentSeat);
+// }
+
+// document.addEventListener("click", handleSeatClickedEvent);
+
+// const seatLeftElement = document.getElementById("seats-left");
+// const seatLeftInnerText = seatLeftElement.innerText;
+// const seatLeft = parseInt(seatLeftInnerText);
+// console.log(typeof seatLeft);
+
+// const newSeatLeft = seatLeft - 1;
+// seatLeftElement.innerText = newSeatLeft;
+
+const allSeat = document.getElementsByClassName("seat-number");
+
+let count = 0;
+
+for (const seat of allSeat) {
+  seat.addEventListener("click", function (event) {
+    const seatLeftElement = document.getElementById("seat-left");
+    const seatLeftInnerText = seatLeftElement.innerText;
+    const seatLeft = parseInt(seatLeftInnerText);
+    count = seatLeft - 1;
+
+    const seatNumber = event.target.parentNode.childNodes[1].innerText;
+    const perSeatPrice = 550;
+    const seatClass = "Economy"
+    setInnerText("seat-left", count);
+
+    console.log(seatNumber);
+  });
 }
 
-document.addEventListener("click", handleSeatClickedEvent);
-
-const seatLeftElement = document.getElementById("seats-left");
-const seatLeftInnerText = seatLeftElement.innerText;
-const seatLeft = parseInt(seatLeftInnerText);
-console.log(typeof seatLeft);
-
-const newSeatLeft = seatLeft - 1;
-seatLeftElement.innerText = newSeatLeft;
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
